3d1b48664cf3e014a365bd30004bb7d9b361e193
Dan Valinotti <dan_valinotti@colpal.com>
2020-02-29 17:33:46 -0500

    add build folder to gitignore

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,7 @@ const next_1 = __importDefault(require("next"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
 const compression_1 = __importDefault(require("compression"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const apollo_1 = __importDefault(require("~server/core/apollo"));
 require('dotenv').config();
 const port = parseInt(process.env.PORT || '3000', 10);
@@ -23,6 +24,8 @@ nextApp.prepare().then(() => {
     const server = express_1.default();
     // security
     server.use(helmet_1.default());
+    // cookies
+    server.use(cookie_parser_1.default());
     // Generate logs
     server.use(morgan_1.default(':method :url :status :res[content-length] - :response-time ms'));
     server.use(compression_1.default());
