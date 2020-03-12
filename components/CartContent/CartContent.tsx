20d4ba1b28cb6394281564f60320bbe323bfd4b3
Dan Valinotti <dan_valinotti@colpal.com>
2020-03-12 15:21:34 -0400

    Set up cart page and link in cart popover

--- components/CartContent/CartContent.tsx
+++ components/CartContent/CartContent.tsx
@@ -1,35 +1,39 @@
 import React, { FunctionComponent } from 'react';
-import { List, ListItem, Typography } from '@material-ui/core';
+import {
+  Button, List, ListItem, Typography,
+} from '@material-ui/core';
 import { GetCartResponse } from '../CartController/_types';
-import './_style.scss';
 import { getLineItems } from '../ProductDetail/ProductDetail';
+import { Styled } from './_styles';
+import Link from "next/link";
 
 type Props = {
   cart: GetCartResponse;
 };
 
-const CartContent: FunctionComponent<Props> = ({ cart }: Props) => {
-  return (
-    <>
-      {cart && (
-        <div id="cart-content">
-          <Typography variant="h6">Cart</Typography>
-          <List className="cart-popover-list">
-            {getLineItems(cart.node.lineItems.edges).map((item, key) => (
-              <ListItem button key={key}>
-                {item.variantId}
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      )}
-      {!cart && (
-        <div id="cart-content">
-          <Typography variant="body2">Loading...</Typography>
-        </div>
-      )}
-    </>
-  );
-};
+const CartContent: FunctionComponent<Props> = ({ cart }: Props) => (
+  <>
+    {cart && (
+      <Styled.Container>
+        <Typography variant="h6">Cart</Typography>
+        <List className="cart-popover-list">
+          {getLineItems(cart.node.lineItems.edges).map((item, key) => (
+            <ListItem button key={key}>
+              {item.variantId}
+            </ListItem>
+          ))}
+        </List>
+        <Link href="/cart">
+          <Button variant="contained" color="secondary">Cart Overview</Button>
+        </Link>
+      </Styled.Container>
+    )}
+    {!cart && (
+      <div id="cart-content">
+        <Typography variant="body2">Loading...</Typography>
+      </div>
+    )}
+  </>
+);
 
 export default CartContent;
---
+++ pages/cart.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+import { Typography, withTheme } from '@material-ui/core';
+import NavBar from '../components/NavBar/NavBar';
+import PageContainer, { PageSize } from '../views/layouts/PageContainer';
+import { withMuiApp } from '../hocs/withMui';
+import { withApollo } from '../services/apollo';
+
+const CartPage = () => (
+  <>
+    <Head>
+      <title>Products</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <NavBar items={['test']} />
+    <PageContainer paddingTop={30} size={PageSize.medium}>
+      <Typography variant="h3">Cart</Typography>
+    </PageContainer>
+  </>
+);
+
+export default withMuiApp(withTheme(withApollo({ ssr: true })(CartPage)));
