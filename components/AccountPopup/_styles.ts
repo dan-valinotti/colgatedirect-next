74e4b92cc027635e49f56e87299bc54945f4dc20
Dan Valinotti <dan_valinotti@colpal.com>
2020-04-03 11:10:08 -0400

    Added AccountPopup component
    Visual elements completed, TODO logout function

---
+++ components/AccountPopup/_styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const IconContainer = styled.div`
+  margin-left: auto;
+`;
+
+const PopoverContentContainer = styled.div`
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  min-width: 300px;
+`;
+
+const PopoverTitle = styled(Typography)`
+  &&& {
+    margin-bottom: 1rem;
+  }
+`;
+
+const EmailText = styled(Typography)`
+  &&& {
+    color: rgba(0,0,0,0.6);
+    margin-top: 0.5rem;
+  }
+`;
+
+const AccountInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding-bottom: 1rem;
+  margin-bottom: 0.5rem;
+  //border-bottom: 1px solid rgba(0,0,0,0.25);
+  width: 100%;
+`;
+
+const AccountActions = styled.div`
+  display: grid;
+  grid-template-columns: 45% 45%;
+  grid-template-rows: 100%;
+  grid-column-gap: 1.5rem;
+  width: 100%;
+`;
+
+export const Styled = {
+  Container,
+  IconContainer,
+  PopoverContentContainer,
+  AccountInfo,
+  PopoverTitle,
+  EmailText,
+  AccountActions,
+};
