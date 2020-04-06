1a7ec1ccb03701af5a890df4c004e8ab340dafaa
Valentina Bustamante <valentinabustamante@mobiledevs-mbp.myfiosgateway.com>
2020-04-06 14:36:33 -0400

    Add image rendering
    Images for each item appears on cart overview page. The variable count updates with incrementCount to use as index in edges array for lineItems.

--- components/CartContentRow/CartContentRow.tsx
+++ components/CartContentRow/CartContentRow.tsx
@@ -4,15 +4,8 @@ import {
   Button, List, ListItem, Typography, Grid, CircularProgress,
 } from '@material-ui/core';
 import withData from '../../lib/apollo';
-import {
-  CREATE_CART,
-  CreateCartResponse,
-  GetCartResponse,
-  GetCartRequest,
-  GET_CART_QUERY,
-  CreateCartRequest,
-  CHECKOUT_LINE_ITEMS_REPLACE_MUTATION,
-} from '../CartController/_types';
+import { GetCartResponse } from '../CartController/_types';
+import './_style.scss';
 import { Styled } from './_styles';
 
 type Props = {
@@ -27,55 +20,68 @@ type Props = {
   getCartRefetch: Function;
 };
 
-const CartContentRow = ({
-  cart, total, getTotal, clearCart, createCartLoading, getCartLoading, createCartError,
-  getCartError, getCartRefetch,
-}: Props) => {
-  const [anchorEl, setAnchorEl] = React.useState(null); // Anchor element
-  const [totalLoading, setTotalLoading] = useState<boolean>(true);
-  const client = useApolloClient();
 
+const CartContentRow = ({
+  cart, total, createCartLoading, getCartLoading, createCartError,
+  getCartError,
+}: Props) => {
+  // index of current item in edges array
+  let count = -1;
+
+  // returns image source url
+  const getImage = (num) => cart.node.lineItems.edges[num].node.variant.image.transformedSrc;
+
+  const incrementCount = () => {
+    count += 1;
+    return count;
+  };
   return (
     <div id="cart-btn">
       {(createCartLoading || getCartLoading) && (
-      <CircularProgress />
+        <CircularProgress />
       )}
       {(createCartError || getCartError) && (
-      <Typography variant="body2">Error!</Typography>
+        <Typography variant="body2">Error!</Typography>
       )}
       {(
         !getCartLoading && !createCartLoading
-      && !getCartError && !createCartError
-      && cart && total !== -1)
-    && (
-    <Styled.Container>
-      <Typography variant="h6">Cart</Typography>
-      <List className="cart-popover-list">
-        {cart.node.lineItems.edges.map((item, key) => (
-          <>
-            <ListItem button key={key}>
-              <Styled.ItemContainer>
-                <Typography variant="h6">{item.node.title}</Typography>
-                <Typography variant="h6">
-                  ${item.node.variant.priceV2.amount * item.node.quantity}
-                </Typography>
-              </Styled.ItemContainer>
-            </ListItem>
-            {key === cart.node.lineItems.edges.length - 1 && (
-            <Styled.CartListItem>
-              <Styled.ItemContainer>
-                <Typography variant="h6">Total:</Typography>
-                <Typography variant="h6">
-                  ${total}
-                </Typography>
-              </Styled.ItemContainer>
-            </Styled.CartListItem>
-            )}
-          </>
-        ))}
-      </List>
-    </Styled.Container>
-    )}
+        && !getCartError && !createCartError
+        && cart && total !== -1)
+        && (
+          <Styled.Container>
+            <List className="cart-popover-list">
+              {cart.node.lineItems.edges.map((item, key) => (
+                <>
+                  <ListItem button key={key}>
+                    <Styled.ItemContainer>
+                      <Styled.ProductImgThumbnail>
+                        <Styled.ProductImg
+                          src={`
+                        ${getImage(incrementCount())}&height=165`}
+                          alt="Thumbnail"
+                        />
+                      </Styled.ProductImgThumbnail>
+                      <Typography variant="h6">{item.node.title}</Typography>
+                      <Typography variant="h6">
+                        ${item.node.variant.priceV2.amount * item.node.quantity}
+                      </Typography>
+                    </Styled.ItemContainer>
+                  </ListItem>
+                  {key === cart.node.lineItems.edges.length - 1 && (
+                    <Styled.CartListItem>
+                      <Styled.ItemContainer>
+                        <Typography variant="h6">Total:</Typography>
+                        <Typography variant="h6">
+                          ${total}
+                        </Typography>
+                      </Styled.ItemContainer>
+                    </Styled.CartListItem>
+                  )}
+                </>
+              ))}
+            </List>
+          </Styled.Container>
+        )}
     </div>
   );
 };
