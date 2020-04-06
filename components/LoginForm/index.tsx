5aa17120a038bd8d34ab2c605d01374743a3b7b6
Dan Valinotti <dan_valinotti@colpal.com>
2020-04-06 09:09:44 -0400

    Removed unused variables and added comment documentation to LoginForm component

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -4,34 +4,45 @@ import validator from 'validator';
 import {
   CircularProgress,
   Dialog, DialogContent,
-  DialogTitle, IconButton,
   InputAdornment,
-  Snackbar, SnackbarContent,
+  Snackbar,
   TextField,
   Typography,
 } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 import { useMutation } from '@apollo/react-hooks';
-import { AccountCircle, VpnKey, Close } from '@material-ui/icons';
+import { AccountCircle, VpnKey } from '@material-ui/icons';
 import { Styled } from './_styles';
 import {
   CustomerLoginQuery, CustomerLoginRequest, CustomerLoginResponse, ErrorStatus,
 } from './_types';
 
+/*
+* LoginForm - component that controls the process of logging in/handling
+* login failures on the /login page.
+* */
 const LoginForm: FunctionComponent = () => {
+  // State variable definitions
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [fieldUpdated, setFieldUpdated] = useState<boolean>(false);
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  // Login error response state
   const [errorStatus, setErrorStatus] = useState<ErrorStatus>({
     status: false,
   });
+  // Login mutation variables
   const [variables, setVariables] = useState<CustomerLoginRequest>({
     email,
     password,
   });
 
-  // GraphQL Requests
+  /*
+  * GraphQL Requests:
+  * submitLoginRequest - mutation that attempts to create a customerAccessToken
+  *   - Request interface:  CustomerLoginRequest
+  *   - Response interface: CustomerLoginResponse
+  * */
   const [submitLoginRequest, { data: submitLoginData }] = useMutation<CustomerLoginResponse>(
     CustomerLoginQuery,
     {
@@ -41,6 +52,7 @@ const LoginForm: FunctionComponent = () => {
     },
   );
 
+  // onClose event for error Snackbar component
   const handleSnackbarClose = () => {
     setErrorStatus({ ...errorStatus, status: false });
   };
@@ -71,7 +83,8 @@ const LoginForm: FunctionComponent = () => {
             res.data.customerAccessTokenCreate.customerAccessToken.accessToken,
           );
           // Go to homepage
-          Router.push('/');
+          Router.push('/')
+            .catch((error) => console.log(error));
         } else {
           // If access token is null, there was an error - set error status
           setErrorStatus({
@@ -91,7 +104,8 @@ const LoginForm: FunctionComponent = () => {
   useEffect(() => {
     // If user is already logged in, go to home page
     if (window && window.localStorage.getItem('customerAccessToken')) {
-      Router.push('/');
+      Router.push('/')
+        .catch((error) => console.log(error));
     }
   }, []);
 
