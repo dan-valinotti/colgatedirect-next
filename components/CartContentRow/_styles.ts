6699d1a35989e451f9e64a3d6e78fb2f2d1fe920
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-04-06 17:36:07 -0400

    Add styling for images on cart overview page
    Image height and width changed. Spacing between item and item title changed.

--- components/CartContentRow/_styles.ts
+++ components/CartContentRow/_styles.ts
@@ -3,20 +3,32 @@ import { ListItem } from '@material-ui/core';
 
 const Container = styled.div`
   padding: 15px;
-  width: min-content;
+  width: 100%;
   margin-right: auto;
   margin-left: auto;
+  align-items: center;
 `;
 
 const ItemContainer = styled.div`
   display: flex; 
-  justify-content: space-between;
   align-items: center;
   width: 100%;
   min-width: 500px;
   
   h6 {
     font-size: 1rem;
+    text-align: right;
+  }
+
+  .itemName {
+    flex-basis: 50%;
+    text-align: left;
+    margin-left: 4rem;
+  }
+
+  .itemPrice {
+    text-align: right;
+    margin-left: 4rem;
   }
 `;
 
@@ -33,10 +45,17 @@ const ProductImg = styled.img`
   max-width: 100%;
   margin: 0 auto;
 `;
+const ProductImgThumbnail = styled.div`
+  height: 50%;
+  width: 20%;
+  display: flex;
+  margin-bottom: 15px;
+`;
 
 export const Styled = {
   Container,
   ItemContainer,
   CartListItem,
   ProductImg,
+  ProductImgThumbnail,
 };
