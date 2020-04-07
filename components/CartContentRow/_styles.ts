5f087765a924b384d72e7a6f884617d6107a3780
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-04-07 12:53:26 -0400

    Add dialog box
    Dialog box will appear after clear cart button is clicked on overview page. Disappears once items are removed from cart. Styling added for box and loading prop passed to CartConentRow.loading indicates if items are currenlty being removed.

--- components/CartContentRow/_styles.ts
+++ components/CartContentRow/_styles.ts
@@ -52,10 +52,18 @@ const ProductImgThumbnail = styled.div`
   margin-bottom: 15px;
 `;
 
+const ProgressContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 0 15px;
+`;
+
 export const Styled = {
   Container,
   ItemContainer,
   CartListItem,
   ProductImg,
   ProductImgThumbnail,
+  ProgressContainer,
 };
