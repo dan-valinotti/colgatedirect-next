871c57f3c4b1fef05e5348786aa9139d3c339268
Dan Valinotti <dan_valinotti@colpal.com>
2020-04-09 10:05:55 -0400

    Add "login" button to AccountPopup if user is not logged in, add first/last name to AccountPopup
    If no "customerAccessToken" entry exists in localStorage a button that links to /login will show in AccountPopup.
    If user is logged in, AccountPopup will now show proper email, and first/last name if those values exist on customer

--- components/AccountPopup/index.tsx
+++ components/AccountPopup/index.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent, useState, useEffect } from 'react';
 import {
-  Button, IconButton, Popover,
+  Button, IconButton, Popover, Typography,
 } from '@material-ui/core';
 import { AccountCircle } from '@material-ui/icons';
+import Link from 'next/link';
 import { Styled } from './_styles';
 import LogoutButton from '../LogoutButton';
 import { withCustomerInfo } from '../../hocs/withCustomerInfo';
@@ -11,6 +12,53 @@ import { CustomerData } from './_types';
 interface Props {
   data: any;
 }
+
+const renderPopoverContent = (
+  customerData: CustomerData,
+  error?: any,
+) => {
+  if (error) {
+    return (
+      <>
+        <Typography variant="body2">An error has occurred.</Typography>
+      </>
+    );
+  } if (customerData) {
+    return (
+      <>
+        <Styled.AccountInfo>
+          <Styled.PopoverTitle variant="h6">Account</Styled.PopoverTitle>
+          <AccountCircle />
+          {(customerData.firstName || customerData.lastName) && (
+            <Typography variant="body1">
+              {customerData.firstName} {customerData.lastName}
+            </Typography>
+          )}
+          <Styled.EmailText variant="body2">
+            { customerData.email }
+          </Styled.EmailText>
+        </Styled.AccountInfo>
+        <Styled.AccountActions>
+          <Button variant="outlined" color="primary">
+            Settings
+          </Button>
+          <LogoutButton />
+        </Styled.AccountActions>
+      </>
+    );
+  } return (
+    <Styled.Container>
+      <Link href="/login">
+        <Button
+          variant="outlined"
+          color="secondary"
+        >
+          Log in
+        </Button>
+      </Link>
+    </Styled.Container>
+  );
+};
 
 const AccountPopup: FunctionComponent<Props> = ({ data }: Props) => {
   const { error, refetch } = data;
@@ -41,46 +89,34 @@ const AccountPopup: FunctionComponent<Props> = ({ data }: Props) => {
 
   return (
     <>
-      {customerData && !error && (
-        <div>
-          <IconButton
-            edge="end"
-            color="inherit"
-            aria-label="cart"
-            onClick={handleClick}
-          >
-            <AccountCircle />
-          </IconButton>
-          <Popover
-            open={open}
-            onClose={handleClose}
-            disableScrollLock
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-          >
-            <Styled.PopoverContentContainer>
-              <Styled.AccountInfo>
-                <Styled.PopoverTitle variant="h6">Account</Styled.PopoverTitle>
-                <AccountCircle />
-                <Styled.EmailText variant="body2">dan33east@gmail.com</Styled.EmailText>
-              </Styled.AccountInfo>
-              <Styled.AccountActions>
-                <Button variant="outlined" color="primary">
-                  Settings
-                </Button>
-                <LogoutButton />
-              </Styled.AccountActions>
-            </Styled.PopoverContentContainer>
-          </Popover>
-        </div>
-      )}
+      <div>
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="cart"
+          onClick={handleClick}
+        >
+          <AccountCircle />
+        </IconButton>
+        <Popover
+          open={open}
+          onClose={handleClose}
+          disableScrollLock
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <Styled.PopoverContentContainer>
+            {renderPopoverContent(customerData, error)}
+          </Styled.PopoverContentContainer>
+        </Popover>
+      </div>
     </>
   );
 };
