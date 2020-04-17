e272e105cf74c340f728c6458d048b8245db597a
Dan Valinotti <dan_valinotti@colpal.com>
2020-04-17 16:25:27 -0400

    Add test cases for AccountPopup component
    Added Cypress test suite for AccountPopup tests
    Changed `npm test` command to open Cypress window

---
+++ cypress/integration/account-popup.spec.js
@@ -0,0 +1,51 @@
+/*
+* AccountPopup Test Suite
+* */
+describe('AccountPopup component tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('.account-btn')
+      .as('iconButton')
+      .click();
+  });
+
+  it.skip('Does component render properly?', () => {
+    cy.get('#account-popup')
+      .should('be.visible');
+  });
+
+  it.skip('Does popup content window open onClick?', () => {
+    cy.get('#popup-content')
+      .should('be.visible');
+  });
+
+  it.skip('Does popup window show "Log in" button when user is NOT logged in?', () => {
+    cy.get('#login-btn')
+      .should('be.visible');
+  });
+
+  it.skip('Does "Log in" button redirect to /login onClick?', () => {
+    cy.get('#login-btn')
+      .click();
+    cy.url()
+      .should('contain', '/login');
+  });
+
+  it('Does popup window show account info when user is logged in?', () => {
+    cy.get('#login-btn')
+      .click();
+    cy.get('#email').type('dan33east@gmail.com');
+    cy.get('#password').type('testing123');
+    cy.get('#form-submit').click();
+    cy.url()
+      .should('include', '/');
+    cy.get('#loading-dialog')
+      .should('not.be.visible', { timeout: 5000 });
+    cy.get('#account-btn')
+      .click() ;
+    cy.get('#account-name')
+      .should('contain', 'Dan Valinotti');
+    cy.get('#account-email')
+      .should('contain', 'dan33east@gmail.com');
+  });
+});
