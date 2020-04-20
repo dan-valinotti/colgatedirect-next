7c60d39ab39e6ff1c287a62e771402cb23915189
Valentina Bustamante <valentinabustamante@mobiledevs-mbp.myfiosgateway.com>
2020-04-20 11:56:53 -0400

    Fix item price to two decimal places on cart overview page

--- components/CartContentRow/CartContentRow.tsx
+++ components/CartContentRow/CartContentRow.tsx
@@ -67,7 +67,7 @@ const CartContentRow = ({
                       <Typography variant="h6" className="itemName">{item.node.title}</Typography>
                       <AddToCart variantId={item.node.variant.id} quantityButton quantity={item.node.quantity} />
                       <Typography variant="h6" className="itemPrice">
-                        ${item.node.variant.priceV2.amount * item.node.quantity}
+                        ${(item.node.variant.priceV2.amount * item.node.quantity).toFixed(2)}
                       </Typography>
                     </Styled.ItemContainer>
                   </ListItem>
