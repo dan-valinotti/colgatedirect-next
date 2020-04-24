30fa47d0764965f314afd49527941aad0e5ed52a
Dan Valinotti <dan_valinotti@colpal.com>
2020-04-24 12:12:29 -0400

    Style PageContentSection to match existing site design
    Added appropriate props and content to PageContentSection to match current site. Needs to have styles added for mobile.

--- components/sections/PageContentSection/_styles.ts
+++ components/sections/PageContentSection/_styles.ts
@@ -1,9 +1,67 @@
 import styled from 'styled-components';
+import { theme } from '../../../views/theme';
 
 const Container = styled.div`
   display: flex;
+  margin-bottom: 4rem;
+  background-color: ${(props) => props.backgroundColor};
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: ${(props) => (props.aligncontent === 'left' ? 'row' : 'row-reverse')};
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  max-width: 300px;
+  height: auto;
+  margin: auto ${(props) => (props.alignWithEdge ? '0' : '1rem')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-basis: 33%;
+  
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const TextContainer = styled.div`
+  display: grid;
+  grid-template-rows: 25% 50% 25%;
+  grid-template-columns: 100%;
+  grid-row-gap: 1rem;
+  flex-basis: 50%;
+  margin: 0 1rem;
+`;
+
+const SectionTitle = styled.h3`
+  color: ${(props) => theme.palette[props.fontcolor].main};
+  font-size: 2.25rem;
+  font-weight: 700;
+  font-family: "Colgate Ready", serif;
+  text-align: left;
+  margin-bottom: 0;
+`;
+
+const SectionSubtitle = styled.p`
+  color: #535353;
+  font-size: 1.25rem;
+  font-family: "Colgate Ready", serif;
 `;
 
 export const Styled = {
   Container,
+  ContentContainer,
+  ImageContainer,
+  TextContainer,
+  SectionTitle,
+  SectionSubtitle,
 };
--- components/sections/PageContentSection/index.tsx
+++ components/sections/PageContentSection/index.tsx
@@ -5,12 +5,54 @@ import CTAButton from '../../CTAButton';
 import { theme } from '../../../views/theme';
 
 type Props = {
-  test?: string;
+  alignContent: string;
+  alignWithEdge: boolean;
+  backgroundColor: string;
+  ctaButton: boolean;
+  ctaButtonText?: string;
+  ctaOnClick?: Function;
+  headline?: string;
+  headlineColor?: string;
+  imageUrl: string;
+  subtitle?: string;
+  title: string;
+  titleColor: string;
 };
 
-const PageContentSection: FunctionComponent<Props> = ({ test }: Props) => (
-  <Styled.Container>
-    <CTAButton color="primary" text="Test button" onClick={() => console.log('click')} />
+const PageContentSection: FunctionComponent<Props> = ({
+  alignContent,
+  backgroundColor,
+  ctaButton,
+  ctaButtonText,
+  ctaOnClick,
+  headline,
+  headlineColor,
+  imageUrl,
+  subtitle,
+  alignWithEdge,
+  title,
+  titleColor,
+}: Props) => (
+  <Styled.Container backgroundColor={backgroundColor}>
+    <Styled.ContentContainer aligncontent={alignContent}>
+      <Styled.ImageContainer alignWithEdge={alignWithEdge}>
+        {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+        <img src={imageUrl} alt="Section Image" />
+      </Styled.ImageContainer>
+      <Styled.TextContainer>
+        <Styled.SectionTitle fontcolor={titleColor}>
+          {title}
+        </Styled.SectionTitle>
+        {subtitle && (
+          <Styled.SectionSubtitle>
+            {subtitle}
+          </Styled.SectionSubtitle>
+        )}
+        {ctaButton && (
+          <CTAButton color="primary" text={ctaButtonText} onClick={ctaOnClick} />
+        )}
+      </Styled.TextContainer>
+    </Styled.ContentContainer>
   </Styled.Container>
 );
 
