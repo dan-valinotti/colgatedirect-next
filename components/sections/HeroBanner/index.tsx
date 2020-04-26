5910659e15987d51e675987e6aab6523baf82066
Dan Valinotti <dan_valinotti@colpal.com>
2020-04-25 23:20:23 -0400

    Fix build time errors

--- components/sections/HeroBanner/index.tsx
+++ components/sections/HeroBanner/index.tsx
@@ -15,13 +15,13 @@ type Props = {
   /** Color of font used by title and subtitle. */
   fontColor: string;
   /** Determines if text is aligned to left or right. */
-  textAlign: 'left' | 'right';
+  textAlign?: 'left' | 'right' | 'center';
   /** Flag to set if "Shop Now" button is placed at bottom of banner. */
   shopNow: boolean;
 };
 
 const HeroBanner: FunctionComponent<Props> = ({
-  title, subtitle, bgColor, bgUrl, fontColor, textAlign, shopNow,
+  title, subtitle, bgColor, bgUrl, fontColor, textAlign = 'center', shopNow,
 }: Props) => (
   <SectionContainer>
     <Styled.Banner imageUrl={bgUrl} bgColor={bgColor} textalign={textAlign}>
