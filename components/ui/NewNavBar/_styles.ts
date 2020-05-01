df5c99ff79991fa4e837f49cbaac56627c6e687b
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-01 15:19:14 -0400

    Added logo w/ link to home for desktop NavBar

--- components/ui/NewNavBar/_styles.ts
+++ components/ui/NewNavBar/_styles.ts
@@ -2,9 +2,10 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   position: fixed;
-  width: 100vw;
+  width: 100%;
   height: 4rem;
   background-color: #ffffff;
+  z-index: 1;
 `;
 
 const FlexContainer = styled.div`
@@ -16,9 +17,48 @@ const FlexContainer = styled.div`
   padding: 0 1rem;
 `;
 
+const Logo = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 12.5rem;
+  height: 12.5rem;
+
+  &:hover {
+    ::before {
+      cursor: pointer;
+    }
+    img {
+      cursor: pointer;
+    }
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    background: #d9291c;
+    left: 0;
+    top: -3rem;
+    width: 12.5em;
+    height: 12.5em;
+    border-radius: 50%;
+    z-index: 2;
+  }
+
+  img {
+    position: absolute;
+    width: 120px;
+    height: auto;
+    left: 1.75rem;
+    z-index: 3;
+    top: 1.75rem;
+  }
+`;
+
 const LogoWrapper = styled.div`
   width: 200px;
   height: 100%;
+  position: relative;
   text-align: center;
   
   * {
@@ -30,4 +70,5 @@ export const Styled = {
   Container,
   FlexContainer,
   LogoWrapper,
+  Logo,
 };
