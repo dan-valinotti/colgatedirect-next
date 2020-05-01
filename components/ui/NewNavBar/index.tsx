29f351b45ebff1980e6c2336336d1492d776c669
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-01 16:41:31 -0400

    Refactored NewNavBarItem to use <Link> components for routing
    This increases performance as Next.js does not reload a new page, just changes page contents based on current href

--- components/ui/NewNavBar/index.tsx
+++ components/ui/NewNavBar/index.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { Styled } from './_styles';
 import { TestInterface } from './_types';
 import NavItems from './navItems.json';
@@ -10,38 +11,31 @@ type Props = {
   test?: string;
 };
 
-const NewNavBar: FunctionComponent<Props> = ({ test }: Props) => {
-  const router = useRouter();
-
-  const toIndex = () => {
-    router.push('/')
-      .catch((error) => console.log(error));
-  };
-
-  return (
-    <Styled.Container>
-      <Styled.FlexContainer style={{ maxWidth: 1100, margin: '0 auto' }}>
-        <Styled.LogoWrapper>
-          <Styled.Logo onClick={toIndex}>
+const NewNavBar: FunctionComponent<Props> = ({ test }: Props) => (
+  <Styled.Container>
+    <Styled.FlexContainer style={{ maxWidth: 1100, margin: '0 auto' }}>
+      <Styled.LogoWrapper>
+        <Styled.Logo>
+          <Link href="/">
             <img src="/static/images/nav-logo.png" alt="ShopSmiles by Colgate" />
-          </Styled.Logo>
-        </Styled.LogoWrapper>
-        <Styled.FlexContainer>
-          {NavItems.navigationItems.map(({ title, handle, products }, key) => (
-            <NewNavBarItem
-              title={title}
-              handle={handle}
-              products={products}
-              key={key}
-            />
-          ))}
-        </Styled.FlexContainer>
-        <Styled.FlexContainer>
-          <NavIconButtons />
-        </Styled.FlexContainer>
+          </Link>
+        </Styled.Logo>
+      </Styled.LogoWrapper>
+      <Styled.FlexContainer>
+        {NavItems.navigationItems.map(({ title, handle, products }, key) => (
+          <NewNavBarItem
+            title={title}
+            handle={handle}
+            products={products}
+            key={key}
+          />
+        ))}
       </Styled.FlexContainer>
-    </Styled.Container>
-  );
-};
+      <Styled.FlexContainer>
+        <NavIconButtons />
+      </Styled.FlexContainer>
+    </Styled.FlexContainer>
+  </Styled.Container>
+);
 
 export default NewNavBar;
