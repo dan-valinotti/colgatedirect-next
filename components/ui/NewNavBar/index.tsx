9c67b33008cb47527580a75750b2dc348674fb49
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-01 15:19:14 -0400

    Added logo w/ link to home for desktop NavBar

--- components/ui/NewNavBar/index.tsx
+++ components/ui/NewNavBar/index.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { useRouter } from 'next/router';
 import { Styled } from './_styles';
 import { TestInterface } from './_types';
 import NavItems from './navItems.json';
@@ -8,24 +9,35 @@ type Props = {
   test?: string;
 };
 
-const NewNavBar: FunctionComponent<Props> = ({ test }: Props) => (
-  <Styled.Container>
-    <Styled.FlexContainer>
-      <Styled.LogoWrapper>
-        <h3>ShopSmiles by Colgate</h3>
-      </Styled.LogoWrapper>
+const NewNavBar: FunctionComponent<Props> = ({ test }: Props) => {
+  const router = useRouter();
+
+  const toIndex = () => {
+    router.push('/')
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <Styled.Container>
       <Styled.FlexContainer>
-        {NavItems.navigationItems.map(({ title, handle, products }, key) => (
-          <NewNavBarItem
-            title={title}
-            handle={handle}
-            products={products}
-            key={key}
-          />
-        ))}
+        <Styled.LogoWrapper>
+          <Styled.Logo onClick={toIndex}>
+            <img src="/static/images/nav-logo.png" alt="ShopSmiles by Colgate" />
+          </Styled.Logo>
+        </Styled.LogoWrapper>
+        <Styled.FlexContainer>
+          {NavItems.navigationItems.map(({ title, handle, products }, key) => (
+            <NewNavBarItem
+              title={title}
+              handle={handle}
+              products={products}
+              key={key}
+            />
+          ))}
+        </Styled.FlexContainer>
       </Styled.FlexContainer>
-    </Styled.FlexContainer>
-  </Styled.Container>
-);
+    </Styled.Container>
+  );
+};
 
 export default NewNavBar;
