cc98e11c033d4831e12e4969bb3a74f5b9fc7585
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-04 11:19:12 -0400

    Add popup onClick for account button

--- components/ui/NavIconButtons/_styles.ts
+++ components/ui/NavIconButtons/_styles.ts
@@ -21,6 +21,7 @@ const IconContainer = styled.div`
   i {
     font-size: 1.25rem;
     color: ${(props) => (props.mobileOpen ? '#fafafa' : 'rgba(0,0,0,0.8)')};
+    position: relative;
     z-index: 5;
     transition: color 0.25s ease-in-out;
 
@@ -35,7 +36,38 @@ const IconContainer = styled.div`
   }
 `;
 
+const AccountButtonContainer = styled.div`
+  position: relative;
+`;
+
+const AccountPopupContainer = styled.div`
+  position: absolute;
+  right: 0;
+  top: 200%;
+  transition: all 0.25s ease-in-out;
+  z-index: 1;
+  transform: ${(props) => (props.open ? 'translateY(0)' : 'translateY(-100%)')};
+  opacity: ${(props) => (props.open ? '1' : '0')};
+  width: 400px;
+  height: 150px;
+  background-color: #fafafa;
+`;
+
+const AccountPopup = styled.div`
+  padding: 1rem;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Colgate Ready', serif;
+`;
+
 export const Styled = {
   Container,
   IconContainer,
+  AccountButtonContainer,
+  AccountPopupContainer,
+  AccountPopup,
 };
