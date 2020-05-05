78ceb3ec0da8403f8e54a07d3c09108fb0a119a6
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-05 10:02:11 -0400

    Remove unused imports/variables from page components

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-import { ParsedUrlQuery } from 'querystring';
 import { ThemeProvider } from '@material-ui/styles';
 import { withTheme } from '@material-ui/core';
 import Head from 'next/head';
 import NewNavBar from 'components/ui/NewNavBar';
-import NavBar from '../components/ui/NavBar/NavBar';
 import { theme } from '../views/theme';
 import { withMuiApp } from '../hocs/withMui';
 import LoginForm from '../components/LoginForm';
 import FooterNav from '../components/FooterNav';
 import { withApollo } from '../lib/apollo';
 
-const LoginPage = () => {
-  const router = useRouter();
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Log In | ShopSmiles® by Colgate</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <NewNavBar />
-      <LoginForm />
-      <FooterNav />
-    </ThemeProvider>
-  );
-};
+const LoginPage = () => (
+  <ThemeProvider theme={theme}>
+    <Head>
+      <title>Log In | ShopSmiles® by Colgate</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <NewNavBar />
+    <LoginForm />
+    <FooterNav />
+  </ThemeProvider>
+);
 
 export default withMuiApp(withTheme(withApollo({ ssr: true })(LoginPage)));
