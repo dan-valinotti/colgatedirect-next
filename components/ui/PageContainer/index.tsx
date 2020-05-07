af8c0e3d38327c2d8ed18ff504e8007338a25e20
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-07 08:54:16 -0400

    Add styled-system and begin building base UI lib

---
+++ components/ui/PageContainer/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import {
+  space, layout, typography, color,
+} from 'styled-system';
+
+const PageContainer = styled.div`
+  ${space}
+  ${layout}
+  ${typography}
+  ${color}
+`;
---
+++ styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    primary: '#d9291c',
+    secondary: '#3b6999',
+  },
+};
+
+export default theme;
