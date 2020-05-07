d54847c4bfd3dc7c4bcbcc30abc47dac3ccabc9b
Dan Valinotti <dan_valinotti@colpal.com>
2020-05-07 09:00:01 -0400

    Fix font imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,6 +49,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
