91581a2493a63a418a55a8cfa079c52a16016526
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-05-25 21:26:20 -0400

    Add the sort button component to the collections page

--- components/sections/CollectionSection/index.tsx
+++ components/sections/CollectionSection/index.tsx
@@ -2,10 +2,12 @@ import React, { FunctionComponent } from 'react';
 import { Styled } from './_styles';
 import { Heading } from '../../ui/Typography/index';
 import NewProductsGrid from '../../ui/NewProductsGrid/index';
+import SortProducts from '../../Collections/SortProducts'
 
 const CollectionSection: FunctionComponent = () => (
   <Styled.Container>
     <Heading tag="h1">Collections</Heading>
+    <SortProducts />
     <Styled.GridContainer>
       <NewProductsGrid />
     </Styled.GridContainer>
