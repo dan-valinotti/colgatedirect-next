cd2a2644498530be14ff156b7b628de5c8d49ea7
Valentina Bustamante <valentinabustamante@Mobiledevs-MacBook-Pro.local>
2020-06-17 13:19:06 -0400

    Add button to go back to top of page when user scrolls down and remove it when user is at the top of the page
    Button does not currently work

--- components/sections/ScrollToTopButton/index.tsx
+++ components/sections/ScrollToTopButton/index.tsx
@@ -1,16 +1,39 @@
-import React, { FunctionComponent } from 'react';
+import React, {
+  FunctionComponent, useState, useEffect, useRef,
+} from 'react';
 import { Styled } from './_styles';
 
 
 const ScrollToTopButton: FunctionComponent = () => {
-  const goToTop = () => {
+  const [isButtonVisible, setIsButtonVisible] = useState<boolean>(false);
 
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300 && !isButtonVisible) {
+      setIsButtonVisible(true);
+    } else if (window.pageYOffset <= 300 && isButtonVisible) {
+      setIsButtonVisible(false);
+    }
   };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', toggleVisibility);
+  });
+
   return (
     <Styled.Container>
-      <Styled.Button onClick={() => goToTop()}>
+      {isButtonVisible && (
+      <Styled.Button onClick={() => scrollToTop}>
         <div>To Top</div>
       </Styled.Button>
+      )}
     </Styled.Container>
   );
 };
